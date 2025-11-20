42107287a90e7094a305cb3089d3f5c06eda96a3
jasonpan0930 <jasonpan0930@gmail.com>
2025-11-20 21:38:08 +0800

    delete outer

--- VGA_RGB_blocks/VGA_clock/vga_clock_inst.v
+++
@@ -1,7 +0,0 @@
-	vga_clock u0 (
-		.clk_clk         (<connected-to-clk_clk>),         //         clk.clk
-		.clock_25_clk    (<connected-to-clock_25_clk>),    //    clock_25.clk
-		.reset_reset_n   (<connected-to-reset_reset_n>),   //       reset.reset_n
-		.clock_74_25_clk (<connected-to-clock_74_25_clk>)  // clock_74_25.clk
-	);
-
