36b6b1f063464b8b7688959bc39c6a8a2d039175
jasonpan0930 <jasonpan0930@gmail.com>
2025-11-21 03:32:50 +0800

    VGA success

--- final/try_VGA_with_COE/qsys_clk/qsys_clk_inst.v
+++
@@ -1,6 +0,0 @@
-	qsys_clk u0 (
-		.clk_clk        (<connected-to-clk_clk>),        //        clk.clk
-		.reset_reset_n  (<connected-to-reset_reset_n>),  //      reset.reset_n
-		.altpll_25m_clk (<connected-to-altpll_25m_clk>)  // altpll_25m.clk
-	);
-
